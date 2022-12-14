a7d90ac9609c7480cbebbddf52a1c836c2bdd307
heesungUm <rehs96@mobmail.shinhan.com>
2022-12-14 19:39:44 +0900

    클립보드 리스트 읽기/쓰기 로직 구현 - 바뀐 primaryClip이 text타입이라면 리스트에 추가 후 저장 - service의 onCreate에서 clipboardList를 읽고, 초기화

--- app/src/main/java/com/heesungum/customkeyboard/KeyboardService.kt
+++ app/src/main/java/com/heesungum/customkeyboard/KeyboardService.kt
@@ -1,15 +1,49 @@
 package com.heesungum.customkeyboard
 
+import android.content.ClipDescription
+import android.content.ClipboardManager
 import android.inputmethodservice.InputMethodService
+import android.util.Log
 import android.view.View
 import com.heesungum.customkeyboard.databinding.KeyboardViewBinding
+import kotlinx.coroutines.runBlocking
+
 
 class KeyboardService : InputMethodService() {
     private lateinit var binding: KeyboardViewBinding
 
+    private lateinit var dataStoreRepository: DataStoreRepository
+    private lateinit var getClipboardListUseCase: GetClipboardListUseCase
+    private lateinit var putClipboardListUseCase: PutClipboardListUseCase
+
+    private var clipboardList: MutableList<String> = mutableListOf()
+
     override fun onCreate() {
         super.onCreate()
         binding = KeyboardViewBinding.inflate(layoutInflater)
+
+        dataStoreRepository = DataStoreRepository(applicationContext)
+        getClipboardListUseCase = GetClipboardListUseCase(dataStoreRepository)
+        putClipboardListUseCase = PutClipboardListUseCase(dataStoreRepository)
+
+        runBlocking {
+            clipboardList = getClipboardListUseCase() ?: mutableListOf()
+            Log.d(TAG, "GetClipboardList: $clipboardList")
+        }
+
+        val manager: ClipboardManager =
+            applicationContext.getSystemService(ClipboardManager::class.java)
+        manager.addPrimaryClipChangedListener {
+            if (manager.primaryClipDescription?.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN) == true) {
+                manager.primaryClip?.getItemAt(0)?.coerceToText(applicationContext)?.toString()
+                    ?.let {
+                        runBlocking {
+                            clipboardList.add(it)
+                            putClipboardListUseCase(clipboardList)
+                        }
+                    }
+            }
+        }
     }
 
     override fun onCreateInputView(): View {
@@ -21,6 +55,10 @@ class KeyboardService : InputMethodService() {
         binding.keyboardFrame.addView(
             koreanKeypadView
         )
-        return binding.keyboardFrame
+        return binding.root
+    }
+
+    companion object {
+        private val TAG: String = KeyboardService::class.java.simpleName
     }
 }
