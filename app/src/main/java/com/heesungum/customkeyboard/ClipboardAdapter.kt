596eb630784334267f4a99c3cc6441662d7b5826
heesungUm <rehs96@mobmail.shinhan.com>
2022-12-14 20:59:13 +0900

    클립보드 리스트 Item 레이아웃, Adpter 구현

---
+++ app/src/main/java/com/heesungum/customkeyboard/ClipboardAdapter.kt
@@ -0,0 +1,55 @@
+package com.heesungum.customkeyboard
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.heesungum.customkeyboard.databinding.ItemClipboardBinding
+
+
+/**
+ *  ClipboardAdapter.kt
+ *
+ *  Created by Heesung Um on 2022/12/14
+ *  Copyright © 2022 Shinhan Bank. All rights reserved.
+ */
+
+class ClipboardAdapter(
+    private val clipboardList: List<String>,
+    private val onItemClick: (Int) -> Unit,
+    private val onDeleteClick: (Int) -> Unit,
+) : RecyclerView.Adapter<ClipboardAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            ItemClipboardBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            onItemClick,
+            onDeleteClick,
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(clipboardList[clipboardList.size - 1 - position])
+    }
+
+    override fun getItemCount(): Int = clipboardList.size
+
+    class ItemViewHolder(
+        private val binding: ItemClipboardBinding,
+        onItemClick: (Int) -> Unit,
+        onDeleteClick: (Int) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                onItemClick.invoke(adapterPosition)
+            }
+            binding.deleteBtn.setOnClickListener {
+                onDeleteClick.invoke(adapterPosition)
+            }
+        }
+
+        fun bind(clipboardText: String) {
+            binding.clipboardTv.text = clipboardText
+        }
+    }
+}
